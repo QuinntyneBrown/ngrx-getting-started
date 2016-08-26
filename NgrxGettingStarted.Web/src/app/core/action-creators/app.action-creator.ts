1ad4a7ff3f38bf47a285de04aec0e3dae01e849e
Quinntyne Brown <quinntynebrown@gmail.com>
2016-08-26 14:59:03 -0400

    observable action creator

--- NgrxGettingStarted.Web/src/app/core/action-creators/app.action-creator.ts
+++ NgrxGettingStarted.Web/src/app/core/action-creators/app.action-creator.ts
@@ -3,6 +3,7 @@ import { Store } from "@ngrx/store";
 import { LOAD_DOCTOR, LOAD_DOCTORS, REMOVE_DOCTOR } from "../actions";
 import { DoctorService } from "../services";
 import { Doctor } from "../models";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class AppActionCreator {
@@ -10,43 +11,49 @@ export class AppActionCreator {
         private _doctorService: DoctorService,
         private _store: Store<any>) { }
 
-    public getAllDoctors = () => {
-        this._doctorService.getAll()
-            .subscribe((data) => {
+    public getAllDoctors(): Observable<boolean> {
+        return this._doctorService
+            .getAll()
+            .map((data) => {
                 this._store.dispatch({
                     type: LOAD_DOCTORS,
                     payload: data
                 });
-            }); 
-    }
-
-    public getById = (options: { id: number }) => {
-        this._doctorService.getById({ id: options.id })
-            .subscribe((data) => {
-                this._store.dispatch({
-                    type: LOAD_DOCTOR,
-                    payload: data
-                });
+                return true;
             });
     }
 
-    public addOrUpdateDoctor = (options: { entity: Doctor}) => {
-        this._doctorService.addOrUpdate({ entity: options.entity })
-            .subscribe((data) => {
+    public getById (options: { id: number }): Observable<boolean> {
+        return this._doctorService.getById({ id: options.id })
+            .map((data) => {
                 this._store.dispatch({
                     type: LOAD_DOCTOR,
                     payload: data
                 });
-            }); 
+                return true;
+            });
     }
 
-    public removeDoctor = (options: { id: number }) => {
-        this._doctorService.remove({ id: options.id })
-            .subscribe((data) => {
+    public addOrUpdateDoctor (options: { entity: Doctor}): Observable<boolean> {
+        return this._doctorService.addOrUpdate({ entity: options.entity })
+            .map((data) => {
+                this._store.dispatch({
+                    type: LOAD_DOCTOR,
+                    payload: data
+                });
+                return true;
+            }); 
+
+    }
+
+    public removeDoctor(options: { id: number }): Observable<boolean> {
+        return this._doctorService.remove({ id: options.id })
+            .map((data) => {
                 this._store.dispatch({
                     type: REMOVE_DOCTOR,
                     payload: data
                 });
+                return true;
             }); 
     }
 }
--- NgrxGettingStarted.Web/src/app/core/utilities/add-or-update.ts
+++ NgrxGettingStarted.Web/src/app/core/utilities/add-or-update.ts
@@ -1,7 +1,7 @@
-﻿export var addOrUpdate = (options: { items: Array<{ id: number }>, item: { id: number }}) => {
+﻿export const addOrUpdate = (options: { items: Array<{ id: number }>, item: { id: number } }) => {
     let exists = false;
     options.items = options.items || [];
-    for (var i = 0; i < options.items.length; i++) {
+    for (let i = 0; i < options.items.length; i++) {
         if (options.items[i].id === options.item.id) {
             options.items[i] = options.item;
             exists = true;
